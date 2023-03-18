1fd1fc4ca55a21835272891f15fa879ec5cec70e
DenisTanev <denistanev@icloud.com>
2023-03-18 19:20:32 +0200

    made the db reset everytime the program is ran

--- ProjectMovieCatalog/Program.cs
+++ ProjectMovieCatalog/Program.cs
@@ -1,6 +1,8 @@
-﻿using ProjectMovieCatalog.Presentation;
+﻿using ProjectMovieCatalog.Data;
+using ProjectMovieCatalog.Presentation;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,16 @@ namespace ProjectMovieCatalog
     {
         static void Main(string[] args)
         {
+            using (var connection = Database.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("DELETE FROM [movies] DBCC CHECKIDENT ('[movies]', RESEED, 0) ", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+
             Display d = new Display();
         }
     }
